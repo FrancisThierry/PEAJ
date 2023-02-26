dc4ea16e3d00da1efa599b04e44510b89a23aef2
Thierry Boulanger <boulangerthie@gmail.com>
2023-02-26 15:00:32 +0100

    first commit python training course

---
+++ PythonLearnig/TD_Loop/functions.py
@@ -0,0 +1,5 @@
+def sum(salaries):
+    sum = -1
+    for i in range(len(salaries)):
+        sum+=salaries[i]
+    return sum
---
+++ PythonLearnig/TD_Loop/main.py
@@ -0,0 +1,5 @@
+from functions import sum
+#programe principal
+
+tabSalaries = [1500,500,4000,7888]
+print(sum(tabSalaries))
